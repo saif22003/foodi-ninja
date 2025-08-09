e6ed37519cf27766dae09148068941f63eef378c
Omar Faruk Shaon <omarfarukshaon2000@gmail.com>
2025-08-09 20:53:08 +0600

    Refactor Customer_Regi_View to use CustomerSerializer

--- backend/customer_app/views.py
+++ backend/customer_app/views.py
@@ -1,27 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Customer_Model
+from .serializers import CustomerSerializer
 
 class Customer_Regi_View(APIView):
     def post(self, request):
-        customer_data = request.data
-
-        customer_info = Customer_Model.objects.create(
-            customer_name = customer_data.get("customer_name"),  
-            customer_phone = customer_data.get("customer_phone"),
-            customer_email = customer_data.get("customer_email"),  
-            gender = customer_data.get("gender"),
-            date_of_birth = customer_data.get("dob"),
-            address =  customer_data.get("address"),
-            password = customer_data.get("password"),  
-        )
-
-        return Response(
-            {"message": "Customer registered successfully in DATABASE."},
-            status=status.HTTP_201_CREATED
-        )
- 
- 
-
-
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Customer registered successfully in DATABASE."},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
